a3c68857adab0b41581506394e4e8aa657feff4b
athril <patryk.orzechowski@gmail.com>
2023-12-12 00:10:58 -0500

    feat: Dynamic versioning

--- src/sparc/client/__init__.py
+++ src/sparc/client/__init__.py
@@ -1,3 +1,4 @@
+from importlib.metadata import version
 from .client import SparcClient
 
 __all__: tuple[str, ...] = [
@@ -5,6 +6,8 @@ __all__: tuple[str, ...] = [
     # "services.pennsieve.PennsieveService"
 ]
 
+
+__version__ = version(__package__)
 
 # import pkg_resources
 # __version__ = pkg_resources.get_distribution("sparc.client").version
