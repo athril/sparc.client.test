c8df9657232c8bb2bdb7df39632d3a1b1eccad90
athril <patryk.orzechowski@gmail.com>
2023-12-12 18:59:23 -0500

    feat: dynamic versioning - works?

--- src/sparc/client/__init__.py
+++ src/sparc/client/__init__.py
@@ -1,11 +1,17 @@
 #import importlib.metadata
 from .client import SparcClient
+from ._version import __version__ 
+#from sparc.client._version import __version__ 
+
+#from _version import __version__
 
 __all__: tuple[str, ...] = [
     "SparcClient",
     # "services.pennsieve.PennsieveService"
 ]
 
+#print('__version__')
+#print(__version__)
 
 #__version__ = importlib.metadata.version(__package__)
 # import pkg_resources
